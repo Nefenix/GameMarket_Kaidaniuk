e3897b436ff636613ac9d76c4bf2a3f13aefcb93
Nefenix <158030014+Nefenix@users.noreply.github.com>
2024-05-13 00:14:07 +0300

    аа
    вї

--- market.js
+++ market.js
@@ -2,14 +2,24 @@ console.log("Перевірка підключення файлу скрипті
 
 let itemsDiv = document.getElementById("items");
 
-console.timeLog(itemsDiv)
-
-if (itemsDiv){
+<<<<<<< HEAD
     console.log(itemsDiv)
+=======
+if (itemsDiv) {
+    console.log(itemsDiv)
+    for (let i = 0; i < 16; i++) {
+        itemsDiv.innerHTML += `<div class = "item">${i + 1}</div>`
+    }
     console.log('Поле classlist: ', itemsDiv.classList)
     console.log('Поле id:', itemsDiv.id)
     console.log('Поле clientWidth:', itemsDiv.clientWidth)
     console.log('Поле innerHTML:', itemsDiv.innerHTML)
-}else {
+
+    itemsDiv.innerText = 'Перший Програмно доданий текст'
+    itemsDiv.innerText += 'Другий Програмно доданий текст'
+
+    itemsDiv.innerText = `<h1>Відформатований HTML код</h1>`
+} else {
     console.log('Блок товарів не знайдено')
 }
+>>>>>>> dc596f848b053ac778f2818d597458c5862cc051
