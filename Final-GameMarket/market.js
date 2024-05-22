5f91f0831a2bb2c731660feb1a0238c428d68099
Nefenix <23a_kiv@liceum.ztu.edu.ua>
2024-05-22 11:17:58 +0300

    sdw
    qdqd

--- Final-GameMarket/market.js
+++ Final-GameMarket/market.js
@@ -1,18 +1,174 @@
 
 let itemsArray = [
     {
-    student: "",
-    developer:"",
-    email: "",
-    githubRepo: "",
-    liveDemo: "",
-    driveLink: "",
-    formLink: "",
-    gameMarketRepo: "",
-    gameMarketDemo: "",
-    marketPage: "",
-}
-]
+        student: "Боровий Артур",
+        developer: "Дід проти москалів",
+        email: "23a_bam@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/23a-bam/Phaser2ndGame",
+        liveDemo: "https://23a-bam.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1A12wx0lfNj7Aef9KAw2CFMaDwhWM8V4Q?usp=sharing",
+        formLink: "https://forms.gle/sS7tuJWJuGDjmHB98",
+        gameMarketRepo: "https://github.com/23a-bam/GameMarket_Borovyi",
+        gameMarketDemo: "https://23a-bam.github.io/GameMarket_Borovyi/",
+        marketPage: "https://23a-bam.github.io/GameMarket_Borovyi/market.html"
+    },
+    {
+        student: "Геча Кирило",
+        developer: "Магазин UkraineRog",
+        email: "23a_gki@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/Kyrylooo/GameMarket_Surname",
+        liveDemo: "https://github.com/Kyrylooo/GameMarket_Surname/settings/pages",
+        driveLink: "https://kyrylooo.github.io/Test1/",
+        formLink: "https://forms.gle/i7YyjwzaHp7QvXei8",
+        gameMarketRepo: "https://github.com/Kyrylooo/Test1",
+        gameMarketDemo: "https://kyrylooo.github.io/Test1/",
+        marketPage: "https://kyrylooo.github.io/Test1/"
+    },
+    {
+        student: "Громова Марина",
+        developer: "",
+        email: "23a_gmo@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/Marina745/Phaser2ndGame",
+        liveDemo: "https://marina745.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1h8T6v1Yt0wkzkiI72lSMMZndaidptT6o",
+        formLink: "https://forms.gle/4tsVxrHtBpkLLmdc6",
+        gameMarketRepo: "https://github.com/Marina745/GameMarket_Hromova/blob/main/market.js",
+        gameMarketDemo: "https://marina745.github.io/GameMarket_Hromova/market.html",
+        marketPage: "https://marina745.github.io/GameMarket_Hromova/market.html"
+    },
+    {
+        student: "Гуменюк Аріна",
+        developer: "",
+        email: "23a_gayu@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/NoraBlank/-Phaser2ndGame?authuser=0",
+        liveDemo: "https://norablank.github.io/-Phaser2ndGame/?authuser=0",
+        driveLink: "https://docs.google.com/document/d/1ESxlcNrGJPWx--NdlFbMdZJNQ3mHWVKQ-HktIVOJBVY/edit?usp=sharing",
+        formLink: "https://forms.gle/rNroPXhu2mFSxcHx8",
+        gameMarketRepo: "",
+        gameMarketDemo: "",
+        marketPage: ""
+    },
+    {
+        student: "Демидчук Ілля",
+        developer: "",
+        email: "23a_dii@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/zxcursed2008/Phaser2ndGame",
+        liveDemo: "https://zxcursed2008.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1BfaZ86pNTA3FO_rOn196XXKOLYNQQ81E",
+        formLink: "https://forms.gle/xs7rgVauTR9ryuNc7",
+        gameMarketRepo: "https://github.com/zxcursed2008/GameMarket_Demydchuk/",
+        gameMarketDemo: "",
+        marketPage: ""
+    },
+    {
+        student: "Деркач Єлизавета",
+        developer: "Царівна жаба",
+        email: "23a_dyem@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/liska228/Phaser2ndGame",
+        liveDemo: "https://liska228.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1uFrc-z8MnRAFvR7sPNM1NnN800TJOUfr",
+        formLink: "https://forms.gle/XDzvrjzJhjq758XFA",
+        gameMarketRepo: "https://github.com/liska228/GameMarket_Derkach-",
+        gameMarketDemo: "https://liska228.github.io/GameMarket_Derkach-/",
+        marketPage: "https://liska228.github.io/GameMarket_Derkach-/market.html"
+    },
+    {
+        student: "Євланов Данііл",
+        developer: "Пригоди Свинки Шмеппи 2",
+        email: "23a_yedv@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/Scoxxy/Phaser2ndGame",
+        liveDemo: "https://scoxxy.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1IzTICIYe3e_rj81-ihjd3oDOz6xfSYQR?usp=drive_link",
+        formLink: "https://forms.gle/vWZ2hn9MopmMy5fc7",
+        gameMarketRepo: "https://github.com/Scoxxy/GameMarket_YevlanovDaniil",
+        gameMarketDemo: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/",
+        marketPage: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/market.html"
+    },
+    {
+        student: "Катя Булич",
+        developer: "Червона відьма",
+        email: "23a_bks@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/katyablchh/Phaser2ndGame",
+        liveDemo: "https://katyablchh.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1bNoYv5ChnzJTrIq0fjekfbqMNjz4SZ40?usp=sharing",
+        formLink: "https://forms.gle/dxTCp9vfCCgZwyy97",
+        gameMarketRepo: "https://katyablchh.github.io/Phaser2ndGame/",
+        gameMarketDemo: "https://katyablchh.github.io/Phaser2ndGame/",
+        marketPage: "https://github.com/katyablchh/GameMarket_Bulych"
+    },
+    {
+        student: "Ковальов Андрій",
+        developer: "They alive",
+        email: "23a_kai@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/Andre00785/Phaser1stGame",
+        liveDemo: "https://github.com/Andre00785/Phaser1stGame/settings/pages",
+        driveLink: "https://docs.google.com/document/d/1D1bRBFti1aHsr-ZAqkfDkK9tdEeOpkM26E9qyWCE8O0/edit?usp=sharing",
+        formLink: "https://forms.gle/i7YyjwzaHp7QvXei8",
+        gameMarketRepo: "https://github.com/Andre00785/Game_Market_Kovalev",
+        gameMarketDemo: "https://andre00785.github.io/Game_Market_Kovalev/",
+        marketPage: "https://andre00785.github.io/Game_Market_Kovalev/market.html"
+    },
+    {
+        student: "Кожанов Юрій",
+        developer: "Catventure",
+        email: "23a_kyuk@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/kyuk278937/Catventure",
+        liveDemo: "",
+        driveLink: "https://drive.google.com/drive/folders/1GM-292zUzJSJlIB4-4_CIoOsHalliizh?usp=sharing",
+        formLink: "https://forms.gle/NN9okEBmpsLw3AmX8",
+        gameMarketRepo: "https://github.com/kyuk278937/GameMarket",
+        gameMarketDemo: "https://kyuk278937.github.io/GameMarket/",
+        marketPage: "https://kyuk278937.github.io/-HomeAppliancesStore/"
+    },
+    {
+        student: "Козлюк Марина",
+        developer: "Пригоди червоної шапочки",
+        email: "23a_kmo@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/Marykozl/Phaser2ndGame",
+        liveDemo: "https://marykozl.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1AZzV2iit4QmQfotZM68HmWo4OjG-5ikO",
+        formLink: "https://forms.gle/PdXpRXZrbn9ujeMH7",
+        gameMarketRepo: "https://github.com/Marykozl/GameMarket_Kozlyuk?hl=ru&authuser=0",
+        gameMarketDemo: "https://marykozl.github.io/GameMarket_Kozlyuk/",
+        marketPage: "https://marykozl.github.io/GameMarket_Kozlyuk/market.html"
+    },
+    {
+        student: "Кравчук Іван",
+        developer: "Пригоди Свиноти",
+        email: "23a_kia@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/maskenissen1/Phaser2ndGame-1",
+        liveDemo: "https://maskenissen1.github.io/Phaser2ndGame-1/",
+        driveLink: "https://drive.google.com/drive/folders/1_qC1jSGlqtfs16m1_nriWvomLg51Ah25",
+        formLink: "https://forms.gle/S2YEZ1dn7tYTkrbF6",
+        gameMarketRepo: "https://github.com/maskenissen1/GameMarket_Kravchuk",
+        gameMarketDemo: "https://maskenissen1.github.io/GameMarket_Kravchuk/",
+        marketPage: "https://maskenissen1.github.io/GameMarket_Kravchuk/market.html"
+    },
+    {
+        student: "Лазаренко Софія",
+        developer: "Mavka",
+        email: "23a_lsa@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/SofiaLazarenko/Phaser2ndGame?authuser=0",
+        liveDemo: "https://sofialazarenko.github.io/Phaser2ndGame/?authuser=0",
+        driveLink: "https://docs.google.com/document/d/1Dqe-8TcUA1YLj7mG-2IMBxHsPmlhkwfkEfrOTAb9Nvs/edit?usp=classroom_web&authuser=0",
+        formLink: "https://forms.gle/PgJv2LH1ZJjjS29V8",
+        gameMarketRepo: "https://github.com/SofiaLazarenko/GameMarket_Lazarenko?authuser=0",
+        gameMarketDemo: "https://sofialazarenko.github.io/GameMarket_Lazarenko/index.html",
+        marketPage: "https://sofialazarenko.github.io/GameMarket_Lazarenko/n/market.html?authuser=0"
+    },
+    {
+        student: "Кайданюк Ілля",
+        developer: "Пригоди Свинки Шмеппи 3",
+        email: "23a_kiv@liceum.ztu.edu.ua",
+        githubRepo: "https://github.com/Nefenix/Phaser2ndGame",
+        liveDemo: "https://nefenix.github.io/Phaser2ndGame/",
+        driveLink: "https://drive.google.com/drive/folders/1xlqWdx6uMj_5XWDlfl4EYZtAD6A8LGMq?usp=drive_link",
+        formLink: "https://forms.gle/e4camX4k2MZvK9qf7",
+        gameMarketRepo: "https://github.com/Nefenix/GameMarket_Kaidaniuk",
+        gameMarketDemo: "https://nefenix.github.io/GameMarket_Kaidaniuk/",
+        marketPage: "https://nefenix.github.io/GameMarket_Kaidaniuk/market.html"
+    }
+];
 
 let itemsDiv = document.getElementById("items");
 // Перевірка існування знайденого блоку 
